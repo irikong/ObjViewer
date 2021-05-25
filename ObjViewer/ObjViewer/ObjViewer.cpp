7905c4f0088d34e3f4c7b62c3fb37ab9cac6a54d
irikong <gookirix@gmail.com>
2021-05-26 02:12:54 +0900

    프로젝트 생성

---
+++ ObjViewer/ObjViewer/ObjViewer.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <gl/glut.h>
+
+int cnt = 0;
+
+void display() {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	switch (cnt % 3) {
+	case 0:
+		glColor3f(1.0f, 0.0f, 0.0f);
+		break;
+	case 1:
+		glColor3f(0.0f, 1.0f, 0.0f);
+		break;
+	case 2:
+		glColor3f(0.0f, 0.0f, 1.0f);
+		break;
+	}
+
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(0.5f, -0.5f);
+	glVertex2f(0.5f, 0.5f);
+	glVertex2f(-0.5f, 0.5f);
+	glEnd();
+
+	//glFlush();
+	glFinish();
+}
+
+void myTimer(int value) {
+	cnt = (cnt + 1) % 10;
+
+	glutTimerFunc(1000, myTimer, 1);
+	glutPostRedisplay();
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Init");
+
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glutDisplayFunc(display);
+	glutTimerFunc(1000, myTimer, 1);
+	glutMainLoop();
+
+	return 0;
+}
