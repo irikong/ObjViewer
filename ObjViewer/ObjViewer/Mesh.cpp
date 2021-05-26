e55559a9ddc6944ebb123c31c218ad0c1efc6f6d
irikong <gookirix@gmail.com>
2021-05-26 22:14:32 +0900

    Read() 추가

--- ObjViewer/ObjViewer/Mesh.cpp
+++ ObjViewer/ObjViewer/Mesh.cpp
@@ -12,6 +12,58 @@ void Mesh::ClearObj()
 
 void Mesh::Read(const char* filename)
 {
+	char line[256];
+	char symbol[5];
+	Vertex v;
+	Texture vt;
+	Normal vn;
+
+	vector<int> vertexIndex, textureIndex, normalIndex;
+	int vIndex, vtIndex, vnIndex;
+
+	FILE* fp = fopen(filename, "r");
+	if (!fp) {
+		cout << "Couldn't open the file!" << endl;
+		exit(1);
+	}
+
+	while (fgets(line, 256, fp)) {
+		sscanf_s(line, "%s", symbol, sizeof(symbol));
+
+		if (!strcmp(symbol, "v")) {
+			sscanf(line, "%*s %f %f %f", &v.x, &v.y, &v.z);
+			vertices.push_back(v);
+		}
+		else if (!strcmp(symbol, "vt")) {
+			sscanf(line, "%*s %f %f", &vt.u, &vt.v);
+			textures.push_back(vt);
+		}
+		else if (!strcmp(symbol, "vn")) {
+			sscanf(line, "%*s %f %f %f", &vn.x, &vn.y, &vn.z);
+			normals.push_back(vn);
+		}
+		else if (!strcmp(symbol, "f")) {
+			char* token = strtok(line, " "); // remove f
+
+			while (token = strtok(NULL, " ")) {
+				if (sscanf(token, "%d/%d/%d", &vIndex, &vtIndex, &vnIndex) == 3) {
+					vertexIndex.push_back(vIndex);
+					textureIndex.push_back(vtIndex);
+					normalIndex.push_back(vnIndex);
+				}
+			}
+			vertexIndices.push_back(vertexIndex);
+			textureIndices.push_back(textureIndex);
+			normalIndices.push_back(normalIndex);
+
+			vertexIndex.clear();
+			textureIndex.clear();
+			normalIndex.clear();
+		}
+		else {
+			continue;
+		}
+	}
 }
 
 void Mesh::Draw()
